bba2d5b239fccd845d3b51c4acefe06f205558db
Luis David <david12.29@hotmail.com>
2024-02-20 16:31:45 -0500

    Create Task models

---
+++ src/models/Task.java
@@ -0,0 +1,11 @@
+package models;
+
+import java.util.Date;
+
+public class Task {
+    private String title;
+
+    private String description;
+    private Date dateExpiration;
+    private boolean completed;
+}
