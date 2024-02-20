91eb9519817d2d9f1175d7d543249ff964b4098d
Luis David <david12.29@hotmail.com>
2024-02-20 16:43:52 -0500

    Update, ConnectionDb model is not finished

--- src/models/ConnectionBD.java
+++ src/models/ConnectionBD.java
@@ -1,5 +1,7 @@
 package models;
 
 public class ConnectionBD {
-    
+    private static final String URL = "jdbc:postgresql://localhost:5432/TaskManagement";
+    private static final String user = "postgres";
+    private static final String password = "12345";
 }
