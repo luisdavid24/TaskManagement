1f40accfc4497647671ce696a783b8f2bd9ff139
Luis David <david12.29@hotmail.com>
2024-02-20 16:46:16 -0500

    Update, ConnectionDb model is improve

--- src/models/ConnectionBD.java
+++ src/models/ConnectionBD.java
@@ -1,7 +1,15 @@
 package models;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionBD {
     private static final String URL = "jdbc:postgresql://localhost:5432/TaskManagement";
-    private static final String user = "postgres";
-    private static final String password = "12345";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "12345";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
