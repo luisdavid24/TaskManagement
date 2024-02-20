da2d5c90eec9e27581adeedc375e8e6f142a80ed
Luis David <david12.29@hotmail.com>
2024-02-20 16:33:17 -0500

    Update, Task models is done

--- src/models/Task.java
+++ src/models/Task.java
@@ -8,4 +8,43 @@ public class Task {
     private String description;
     private Date dateExpiration;
     private boolean completed;
+
+    public Task(String title, String description, Date dateExpiration, boolean completed) {
+        this.title = title;
+        this.description = description;
+        this.dateExpiration = dateExpiration;
+        this.completed = completed;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDateExpiration() {
+        return dateExpiration;
+    }
+
+    public void setDateExpiration(Date dateExpiration) {
+        this.dateExpiration = dateExpiration;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
